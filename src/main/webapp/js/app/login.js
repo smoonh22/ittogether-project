f251ce78cdc5482084bbc9206e2f519e0d10926e
moonhyucksong <smoonh22@gmail.com>
2015-09-18 13:45:06 +0900

    금요일 오후 1시 45분

--- src/main/webapp/js/app/login.js
+++ src/main/webapp/js/app/login.js
@@ -15,20 +15,19 @@ function login() {
 
 
 $('#signup-button').click(function (event) {
-  $.ajax('auth/signup.do',
-    {
-      method: 'POST',
-      dataType: 'json',
-      data: {
-        email: $('#email').val(),
-        nickname: $('#nickname').val(),
-        password: $('#password').val(),
-        name: $('#name').val(),
-        address: $('#pac-input').val(),
-        latitude: $('#latitude').val(),
-        longitude: $('#longitude').val()
-      },
-  success: function (result) {
+  $.ajax('auth/signup.do', {
+    method: 'POST',
+    dataType: 'json',
+    data: {
+      email: $('#email').val(),
+      nickname: $('#nickname').val(),
+      password: $('#password').val(),
+      name: $('#name').val(),
+      address: $('#pac-input').val(),
+      latitude: $('#latitude').val(),
+      longitude: $('#longitude').val()
+    },
+    success: function (result) {
       if (result.result === 'success') {
         console.log('가입 축하드립니다.');
         $('#normalModal').modal('toggle');
@@ -41,69 +40,80 @@ $('#signup-button').click(function (event) {
 // 지도 관련 시작 
 function initAutocomplete() { /* 초기화 함수*/
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.497943, lng: 127.027622}, /* 처음 지도에서 비춰지는 지역 */
+    center: {
+      lat: 37.497943,
+      lng: 127.027622
+    },
+    /* 처음 지도에서 비춰지는 지역 */
     zoom: 15,
     disableDefaultUI: true
   });
-  
+
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  map.addListener('bounds_changed', function() {
+  map.addListener('bounds_changed', function () {
+    
     searchBox.setBounds(map.getBounds());
+    
   });
-  
-  var marker;
-  var markers = [];
+
+   var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
   searchBox.addListener('places_changed', function() {
     var places = searchBox.getPlaces();
 
-   /*  markers.forEach(function(marker) {
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
       marker.setMap(null);
     });
     markers = [];
- */
+
+    // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
+
+      // Create a marker for each place.
       markers.push(new google.maps.Marker({
+        map: map,
         title: place.name,
-        position: place.geometry.location,
-        map: map
+        position: place.geometry.location
       }));
 
       if (place.geometry.viewport) {
+        // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
       } else {
         bounds.extend(place.geometry.location);
       }
     });
     map.fitBounds(bounds);
-    map.setZoom(16); /* 검색 후 비춰지는 지도의 줌 정도 */
-  });
-  
-  google.maps.event.addListener(map, 'click', function(event) {
+    map.setZoom(18);
+    $("#latitude").val(markers[0].position.H); /* 위도 경도 */
+    $("#longitude").val(markers[0].position.L);
+  google.maps.event.addListener(map, 'click', function (event) {
     addMarker(event.latLng, map); /* 마커 찍어주는 함수 */
   });
 
-  function addMarker(location, map) { 
-    if (marker) {
-      marker.setPosition(location);
-    } else {
-        markers[0].setPosition(null);
-        marker = new google.maps.Marker({
-          position: location,
-          map: map
-        });
-    }
-    $("#latitude").val(marker.position.H); /* 위도 경도 */
-    $("#longitude").val(marker.position.L);
+function addMarker(location, map) {
+    markers[0].setPosition(location);
+    $("#latitude").val(markers[0].position.H); /* 위도 경도 */
+    $("#longitude").val(markers[0].position.L);
   }
+  });
+
 }
+
 
 // 모달창에서 구글맵 나오게 trigger
 $('#normalModal').on("shown.bs.modal", function () {
-    google.maps.event.trigger(map, "resize");
+  google.maps.event.trigger(map, "resize");
 });
 
 // 지도 관련 끝
