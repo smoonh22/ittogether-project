142cd9666028f0690ae65e6a4becb3902445aced
moonhyucksong <smoonh22@gmail.com>
2015-09-21 10:59:20 +0900

    fff

--- src/main/webapp/js/controllers/noneFriendsFeed.js
+++ src/main/webapp/js/controllers/noneFriendsFeed.js
@@ -13,7 +13,8 @@ define(function() {
       date:'오늘저녁6시',
       headcount: '1/2',
       pasttime: '1 hours ago',
-      content: '냄비 필요하신분 있으신가요?? 깨끗하고 거의 안썼어요'
+      content: '냄비 필요하신분 있으신가요?? 깨끗하고 거의 안썼어요',
+      category: 1
     },
     {
       name: '한지민',
@@ -21,7 +22,8 @@ define(function() {
       date:'오늘저녁6시30분',
       headcount: '0/2',
       pasttime: '30 minutes ago',
-      content: '고향에서 어머니가 반찬을 보내주시는데, 이번주 내내 출장가있어서 받지를 못하네요.. 받아주실분! 부탁드려요!!'
+      content: '고향에서 어머니가 반찬을 보내주시는데, 이번주 내내 출장가있어서 받지를 못하네요.. 받아주실분! 부탁드려요!!',
+      category: 2
       
     },
     {
@@ -30,7 +32,8 @@ define(function() {
       date:'오늘저녁8시30분',
       headcount: '1/3',
       pasttime: '30 minutes ago',
-      content: '심야영화 볼사람 있나요? 베테랑 같이봐요! 끝나고 맥주 간단하게 한잔 콜?'
+      content: '심야영화 볼사람 있나요? 베테랑 같이봐요! 끝나고 맥주 간단하게 한잔 콜?',
+      category: 3
       
     }
   ];
