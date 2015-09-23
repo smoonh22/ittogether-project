d26e9d89437c2c6ad63d45423e5c66516fb4dd75
moonhyucksong <smoonh22@gmail.com>
2015-09-23 16:52:00 +0900

    최신

---
+++ src/main/webapp/js/controllers/myFriendList.js
@@ -0,0 +1,41 @@
+define(function() {
+  var app = angular.module('myFriendList', []);
+  
+app.controller('myFriendCtrl',['$http','$scope', function($http,$scope) {
+    var parent_scope = this;
+    $http.get('frd/list.do',
+         {
+        params: {
+          mno: sessionStorage.getItem('mno')
+        }
+    }).success(function(result){
+      console.log("히히히 : " + result.data);
+      console.log("하하하: " + result.apply);
+      parent_scope.frdLists = result.data;
+      parent_scope.applyLists = result.apply;
+      
+    });
+    // 삭제버튼
+    $scope.delfrd = function () {
+      $http.get('frd/delete.do', { frdmno : parent_scope.delfrdmno },
+          function(response) {
+            console.log("dbdbdbd");
+          },
+          function(failure) {
+            console.log("failed :(", failure);
+            });
+    }
+    // 수락 버튼
+  /*  $scope.applyfrd = function () {
+      $http.get('frd/delete.do', { frdmno : parent_scope.delfrdmno },
+          function(response) {
+        console.log("dbdbdbd");
+      },
+      function(failure) {
+        console.log("failed :(", failure);
+      });
+    }*/
+    
+  
+  }]);
+});
