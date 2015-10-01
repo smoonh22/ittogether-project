6f95d2de7e9cf0ee2458043bcb7246b2579c512c
weritas247 <secondwarre@gmail.com>
2015-10-01 18:11:51 +0900

    ㅇㅇ

---
+++ src/main/webapp/js/app/info-modal.js
@@ -0,0 +1,12 @@
+$(function () {
+$(function () {
+    $('#fileupload1').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.data, function (index, file) {
+                $('#profile-img').attr('src', file.url);
+            });
+        }
+    });
+});
+});
