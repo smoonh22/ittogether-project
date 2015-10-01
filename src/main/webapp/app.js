e9efc10d94459bc8b8d7ffc3c0a02acccc670c9d
weritas247 <secondwarre@gmail.com>
2015-10-01 18:11:10 +0900

    10월 1일 저녁 먹기 전 마감

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -8,21 +8,25 @@
  */
 requirejs.config({
     paths: {
-        //라이브러리
-        'jquery': 'bower_components/jquery/dist/jquery',
-        'angular': 'bower_components/angular/angular',
-        'bootstrap.min': 'bower_components/bootstrap/dist/js/bootstrap',
-        'bootstrap-tour': 'bower_components/bootstrap-tour/build/js/bootstrap-tour',
-        'angular-route': 'bower_components/angular-route/angular-route',
-        //컨트롤러
-        'main': 'js/app/main',
-        'myFriendList': 'js/app/myFriendList',
-        'friendsFeed': 'js/app/friendsFeed',
-        'tour': 'js/app/tour',
-        'myActivities': 'js/app/myActivities',
-        'customMyMap' : 'js/app/myMap',
-        'mainMap' : 'js/app/main_map',
-        'myFriendList': 'js/app/myFriendList'
+      //라이브러리
+      'jquery': 'bower_components/jquery/dist/jquery',
+      'angular': 'bower_components/angular/angular',
+      'bootstrap.min': 'bower_components/bootstrap/dist/js/bootstrap',
+      'bootstrap-tour': 'bower_components/bootstrap-tour/build/js/bootstrap-tour',
+      'angular-route': 'bower_components/angular-route/angular-route',
+      'jquery_ui_widget' : 'bower_components/blueimp-file-upload/js/vendor/jquery.ui.widget',
+      'jquery-iframe-transport': 'bower_components/blueimp-file-upload/js/jquery.iframe-transport',
+      'jquery-fileupload': 'bower_components/blueimp-file-upload/js/jquery.fileupload',
+      //컨트롤러
+      'main': 'js/app/main',
+      'myFriendList': 'js/app/myFriendList',
+      'friendsFeed': 'js/app/friendsFeed',
+      'tour': 'js/app/tour',
+      'myActivities': 'js/app/myActivities',
+      'customMyMap' : 'js/app/myMap',
+      'mainMap' : 'js/app/main_map',
+      'myFriendList': 'js/app/myFriendList',
+      'info-modal': 'js/app/info-modal'
     },
   
   shim: {
@@ -43,7 +47,7 @@ requirejs.config({
       deps:['jquery']
     },
     'main': {
-      deps: ['angular-route', 'friendsFeed', 'bootstrap.min', 'tour','myFriendList','myActivities','mainMap', 'customMyMap']
+      deps: ['angular-route', 'friendsFeed', 'bootstrap.min', 'tour','myFriendList','myActivities','mainMap', 'customMyMap', 'jquery_ui_widget', 'jquery-iframe-transport', 'jquery-fileupload']
     },
     'friendsFeed': {
       deps: ['angular']
@@ -58,7 +62,10 @@ requirejs.config({
       deps: ['angular']
     },
     'myFriendList': {
-      deps:['angular']
+      deps: ['angular']
+    },
+    'info-modal': {
+      deps: ['jquery_ui_widget', 'jquery-iframe-transport', 'jquery-fileupload']
     }
   }
   
